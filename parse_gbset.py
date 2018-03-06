c984e0317da6d37603e4c16a612706f81b2397bb
Steven Lakin <steven.m.lakin@gmail.com>
2018-03-06 09:12:54 -0500

    Change from symlink to file

--- parse_gbset.py
+++ parse_gbset.py
@@ -1,1 +1,48 @@
-/home/lakinsm/PycharmProjects/usda_scripts/parse_gbset.py
+#!/usr/bin/env python3
+
+import sys
+import glob
+from Bio import Entrez
+
+
+def multi_gb_to_fasta(in_dir):
+    for file in glob.glob(in_dir + '/*'):
+        with open(file, 'r') as f:
+            records = Entrez.parse(f)
+            for record in records:
+                acc = record['GBSeq_accession-version']
+                for feature in record['GBSeq_feature-table']:
+                    gene = None
+                    product = None
+                    transl = None
+                    if 'GBFeature_quals' in feature:
+                        quals = feature['GBFeature_quals']
+                    else:
+                        continue
+                    for qual in quals:
+
+                        if qual['GBQualifier_name'] == 'gene':
+                            gene = qual['GBQualifier_value']
+                        elif qual['GBQualifier_name'] == 'product':
+                            product = qual['GBQualifier_value']
+                        elif qual['GBQualifier_name'] == 'translation':
+                            transl = qual['GBQualifier_value']
+                        else:
+                            continue
+                    if gene and product and transl:
+                        sys.stdout.write('>{} ~~~{}~~~{}\n{}\n'.format(
+                            acc,
+                            gene,
+                            product,
+                            transl
+                        ))
+                    elif product and transl and not gene and product != 'hypothetical protein':
+                        sys.stdout.write('>{} ~~~~~~{}\n{}\n'.format(
+                            acc,
+                            product,
+                            transl
+                        ))
+
+
+if __name__ == "__main__":
+    multi_gb_to_fasta(sys.argv[1])
--- separate_xml.py
+++ separate_xml.py
@@ -1,1 +1,23 @@
-/home/lakinsm/PycharmProjects/usda_scripts/separate_xml.py
+#!/usr/bin/env python
+
+import sys
+
+
+def separate_xml(file, out_dir):
+    with open(file, 'r') as f:
+        data = f.read().split('\n')
+        num = 0
+        for line in data:
+            if line.startswith('<?xml version') and num == 0:
+                out = open(out_dir + '/gbk_' + str(num) + '.xml', 'w')
+                num += 1
+            elif line.startswith('<?xml version') and num > 0:
+                out.close()
+                num += 1
+                out = open(out_dir + '/gbk_' + str(num) + '.xml', 'w')
+            out.write('{}\n'.format(line))
+        out.close()
+
+
+if __name__ == '__main__':
+    separate_xml(sys.argv[1], sys.argv[2])
